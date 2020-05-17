b78f5af95eb4ede82a40708f230b8b70acb10e03
Samyak Lal <samyak.lal@nagarro.com>
2020-05-17 18:58:35 +0530

    Fixed login component's unit testing issue.

--- src/app/login/login.component.spec.ts
+++ src/app/login/login.component.spec.ts
@@ -1,5 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { RouterTestingModule } from '@angular/router/testing';
+
 import { LoginComponent } from './login.component';
 
 describe('LoginComponent', () => {
@@ -8,6 +10,7 @@ describe('LoginComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [RouterTestingModule],
       declarations: [ LoginComponent ]
     })
     .compileComponents();
