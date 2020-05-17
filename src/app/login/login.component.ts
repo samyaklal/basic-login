e003759b16745f6900662e794c503c1f06ad4e98
Samyak Lal <samyak.lal@nagarro.com>
2020-05-17 22:09:43 +0530

    Flexible and multiple credentials support.

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
+const credentials = {
+  admin: "admin",
+  username: "password"
+};
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -17,7 +22,7 @@ export class LoginComponent implements OnInit {
   }
 
   login(): void {
-    if(this.userName === "admin" && this.password === "admin") {
+    if(credentials[this.userName] && this.password === credentials[this.userName]) {
       this.showError = false;
       this.router.navigate(["dashboard"]);
     } else {
